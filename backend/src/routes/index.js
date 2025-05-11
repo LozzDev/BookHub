22b0ec978d1bacd7e0df9b60c0b80525284225b9
LozzDev <lozzdeveloper@gmail.com>
2025-05-11 19:52:24 +0200

    add: create middleware auth file and applied on routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const booksRouter = require('./book.routes');
 const userRouter = require('./user.routes');
+const authMiddleware = require('../middleware/auth.middleware');
 
-router.use('/books', booksRouter);
-router.use('/users', userRouter);
+router.use('/books', authMiddleware, booksRouter);
+router.use('/users', authMiddleware, userRouter);
 
 
 module.exports = router;
