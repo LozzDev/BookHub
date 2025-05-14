701104263cf4c29aa0baa3690812a1b660877ae7
LozzDev <lozzdeveloper@gmail.com>
2025-05-14 23:45:00 +0200

    add: basic content Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div>Header</div>
+      <div className='unlogged-content flex'>
+        <div>
+          <h1>BOOKHUB</h1>
+          <p>Lee, descarga y disfruta.</p>
+          <button>Regístrate</button>
+        </div>
+        <div className='book-image-container'>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
