5d21747016c7d1d4fcd1d7b0378e71671ac0627a
LozzDev <lozzdeveloper@gmail.com>
2025-05-15 00:26:48 +0200

    add: get all books fetch in Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,20 +4,39 @@ import { useEffect, useState } from 'react';
 
 const Home = () => {
 
+  const [books, setBooks] = useState([]);
+
   useEffect( () => {
     const fetchData = async () => {
       try{
-        const Data = fetch(`${import.meta.env.VITE_API_VITE}/bookhub/books`,
+        const response = await fetch(`http://localhost:3000/bookhub/books`,
           {
             method: 'GET',
-            headers: 'application/json'
+            headers: {
+              'Content-Type': 'application/json'
+            }
           }
-        )
+        );
+
+        if(response.ok){
+          const data = await response.json();
+          setBooks(data); 
+        }else {
+          console.error('error obteniendo los libros:', response.statusText);
+        }
       }catch(error){
         console.error('error obteniendo los libros: ' , error);
       }
     }
+
+    fetchData();
+
   }, [])
+
+  const handleDetailsClick = (book) => {
+    console.log("Detalles del libro:", book);
+    // en esta función hay que poner el navigate a la pagina de detalles del libro
+  };
 
   return (
     <div>
@@ -31,16 +50,27 @@ const Home = () => {
         <div className='book-image-container'>
           <img src="" alt="" />
         </div>
-        <div className='catalog'>
+      </div>
+      <div className='catalog'>
           <h2>Catálogo</h2>
           <div className='filters flex'>
             <h2>*Filtros*</h2>
-            {
-
-            }
+            {books.length > 0 ? (
+              books.map((book) => (
+                <BookCard
+                  key={book._id} 
+                  image={book.coverImage}
+                  title={book.title}
+                  author={book.author}
+                  onDetailsClick={() => handleDetailsClick(book)}
+                />
+              ))
+            ) : (
+              <p>No hay libros disponibles.</p>
+            )}
           </div>
-        </div>
       </div>
+      
     </div>
   )
 }
