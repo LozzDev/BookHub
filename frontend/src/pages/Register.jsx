4bb000328907197a7ca8bc40c0b0d6de6846f9e9
LozzDev <lozzdeveloper@gmail.com>
2025-05-24 19:12:11 +0200

    add: basci structure and styles to Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+      <Header/>
+        <div
+        className="flex justify-center items-center min-h-screen"
+        >
+        <div className='bg-black/20 flex flex-col w-96 items-center rounded-2xl p-7 shadow-2xl'>
+          <p className='text-xl mb-4 text-black font-medium'>Registrarse</p>
+          <form action="" className='flex flex-col gap-4 w-full'>
+            <label className='text-black'>Email</label>
+            <input type="text" className='bg-white rounded-md p-2 shadow' />
+            <label className='text-black'>Usuario</label>
+            <input type="text" className='bg-white rounded-md p-2 shadow' />
+            <label className='text-black'>Contraseña</label>
+            <input type="password" className='bg-white rounded-md p-2 shadow' />
+            <label className='text-black'>Repetir contraseña</label>
+            <input type="password" className='bg-white rounded-md p-2 shadow' />
+            <button className='bg-black text-white pt-4 pb-4 rounded-2xl mt-4 cursor-pointer'>
+              Iniciar sesión
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
