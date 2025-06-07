1ccba4f1453731129e1b43b8f0e66e4e688ec8bc
LozzDev <lozzdeveloper@gmail.com>
2025-06-07 22:50:05 +0200

    add: BookDetails styles

--- frontend/src/pages/BookDetails.jsx
+++ frontend/src/pages/BookDetails.jsx
@@ -34,40 +34,45 @@ const BookDetails = () => {
   return (
     <>
     <Header/>
-    <div className="min-h-screen bg-gradient-to-b from-gray-100 to-gray-300 p-6 flex justify-center items-center">
+    <div className="p-3 flex justify-center items-center">
       
-      <div className="bg-[#f5f1ec] shadow-xl rounded-lg p-6 max-w-6xl w-full flex gap-8">
+      <div className=" rounded-lg  max-w-6xl w-full flex flex-col lg:flex-row items-center gap-8">
         {/* Portada del libro */}
-        <div className="w-1/2 flex justify-center items-start">
-          <div className="w-full max-w-md shadow-lg rounded overflow-hidden">
-            <img
-              src={book.coverImage}
-              alt="cover-image"
-              className="w-full h-auto rounded"
-            />
-          </div>
+        <div className="w-full  flex  justify-center items-start">
+          <img
+            src={book.coverImage}
+            alt="cover-image"
+            className="rounded-2xl h-[500px] lg:h-[750px] object-cover"
+          />
         </div>
 
-        {/* Información del libro */}
-        <div className="w-1/2 flex flex-col justify-between">
-          <div className="bg-[#e5dfd6] rounded-lg p-6 shadow-inner">
-            <h1 className="text-3xl font-bold mb-2 text-gray-800">{book.title}</h1>
-            <p className="text-sm text-gray-600 mb-4">{book.author}</p>
+{/* Información del libro */}
+<div className="w-full flex flex-col items-center h-[650px] lg:h-[750px] justify-between">
+  <div className="flex flex-col p-6 w-11/12 bg-black/20 rounded-3xl items-center h-[85%]">
+    <h1 className="text-3xl font-bold mb-2 text-center">{book.title}</h1>
+    <p className="text-sm mb-4 text-center">{book.author}</p>
 
-            <h2 className="text-xl font-semibold text-gray-700 mb-2">Sinopsis</h2>
-            <p className="text-gray-700 leading-relaxed whitespace-pre-line">{book.description}</p>
-          </div>
+    <h2 className="text-xl font-semibold mb-2">Sinopsis</h2>
 
-          {/* Botones */}
-          <div className="flex justify-center mt-6 gap-4 ">
-            <button className="bg-black text-white px-4 py-2 rounded-full hover:bg-gray-800 transition-all cursor-pointer">
-              Descargar
-            </button>
-            <button className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-all flex items-center gap-2 cursor-pointer">
-              Leer
-            </button>
-          </div>
-        </div>
+    {/* Contenedor con scroll vertical para la sinopsis */}
+    <div className="overflow-y-auto max-h-[400px] lg:max-h-[500px] w-full px-2 overflow-x-hidden">
+  <p className="leading-relaxed whitespace-pre-line text-sm text-justify break-all max-w-full">
+    {book.description}
+  </p>
+</div>
+  </div>
+
+  {/* Botones */}
+  <div className="flex justify-center mt-4 gap-4 mb-2">
+    <button className="bg-black text-white px-4 py-2 rounded-full hover:bg-gray-800 transition-all cursor-pointer">
+      Descargar
+    </button>
+    <button className="bg-blue-600 text-white px-6 py-2 rounded-full hover:bg-blue-700 transition-all flex items-center gap-2 cursor-pointer">
+      Leer
+    </button>
+  </div>
+</div>
+
       </div>
     </div>
     </>
